4c3818098a405b007329d777dfc36e27b4bfa2f2
Andres Felipe Murcia Zapata <andresf.murciaz@gmail.com>
2025-02-11 20:38:26 -0500

    consumo de API'S

---
+++ packages/04-pruebatecnica01/README.md
@@ -0,0 +1,6 @@
+Prueba tecnica 
+APIS:
+-Facts Random : https://catfact.ninja/fact
+-Imagen randon : https://cataas.com/cat/says/hello?size=50&color=red&json=true
+
+Recupera un hecho aleatorio de gatos y muestra una imagen de un gato con la primera palabra del hecho
---
+++ packages/04-pruebatecnica01/src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+
+function App() {
+  const [fact, setFact] = useState('Cargando...')
+  const [imgURL, setImgUrl] = useState("")
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        // Obtengo las tres primeras palabras del fact
+        const firstWords = fact.split(' ').slice(0, 3).join(' ')
+
+        // Genero la URL de la imagen directamente
+        setImgUrl(`https://cataas.com/cat/says/${encodeURIComponent(firstWords)}`)
+      })
+      .catch(error => console.error("Error al obtener el hecho del gato:", error))
+  }, [])
+
+  return (
+    <main>
+      <h1 >Gatos</h1>
+      <p>{fact}</p>
+      {imgURL && <img src={imgURL} alt="Gato aleatorio" />}
+    </main>
+  )
+}
+
+export default App
